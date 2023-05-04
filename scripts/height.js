f672b6ff05a762fbd7df046a7a4f345513f0c69a
nicholas <nicholasciano01@gmail.com>
2023-05-04 16:29:42 +1000

    Fixed dynamic background image resize

---
+++ scripts/height.js
@@ -0,0 +1,10 @@
+window.addEventListener("resize", heightEquivalence);
+
+function heightEquivalence() {
+    var gridHeight = document.getElementsByClassName("homegrid")[0].scrollHeight;
+    var imageHeight = document.getElementsByClassName("backdrop-image")[0].scrollHeight;
+    console.log(gridHeight);
+    console.log(imageHeight);
+    document.getElementsByClassName("backdrop-image")[0].style.height = gridHeight + "px";
+    document.getElementsByClassName("backdrop-image")[1].style.height = gridHeight + "px";
+}
