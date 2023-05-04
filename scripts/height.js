00d2ba786b5098636511f9bbcd578fd8af9e5c6d
nicholas <nicholasciano01@gmail.com>
2023-05-04 17:19:20 +1000

    Fixed so home page content fits viewport with nav

--- scripts/height.js
+++ scripts/height.js
@@ -2,6 +2,9 @@ window.addEventListener("load", heightEquivalence);
 window.addEventListener("resize", heightEquivalence);
 
 function heightEquivalence() {
+    var navHeight = document.getElementsByTagName("header")[0].scrollHeight;
+    document.getElementsByClassName("homegrid")[0].style.height = "calc(100vh - " + navHeight + "px)";
+    console.log("calc(100vh - " + navHeight + "px)");
     var gridHeight = document.getElementsByClassName("homegrid")[0].scrollHeight;
     var imageHeight = document.getElementsByClassName("backdrop-image")[0].scrollHeight;
     console.log(gridHeight);
