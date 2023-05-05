3e344337229ea49004cd584e705b3a90911ea5e2
nicholas <nicholasciano01@gmail.com>
2023-05-05 14:20:49 +1000

    Fixed footer overflow on homepage

--- scripts/height.js
+++ scripts/height.js
@@ -4,11 +4,13 @@ window.addEventListener("resize", heightEquivalence);
 function heightEquivalence() {
     var navHeight = document.getElementsByTagName("header")[0].scrollHeight;
     document.getElementsByClassName("homegrid")[0].style.height = "calc(100vh - " + navHeight + "px)";
-    console.log("calc(100vh - " + navHeight + "px)");
     var gridHeight = document.getElementsByClassName("homegrid")[0].scrollHeight;
     var imageHeight = document.getElementsByClassName("backdrop-image")[0].scrollHeight;
-    console.log(gridHeight);
-    console.log(imageHeight);
+    
+    var gridHeightNoClip = document.getElementsByClassName("homegrid")[0].offsetHeight;
+    var gridHeightClip =  gridHeight - gridHeightNoClip;
+    console.log(gridHeightClip);
     document.getElementsByClassName("backdrop-image")[0].style.height = gridHeight + "px";
     document.getElementsByClassName("backdrop-image")[1].style.height = gridHeight + "px";
+    document.getElementsByTagName("footer")[0].style.transform = "translateY(" + gridHeightClip + "px)";
 }
