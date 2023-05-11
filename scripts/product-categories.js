9453073d9aecc69419492cbd562a2d243fd73d12
nicholas <nicholasciano01@gmail.com>
2023-05-11 16:54:56 +1000

    Category button clicked twice; go back to popular

--- scripts/product-categories.js
+++ scripts/product-categories.js
@@ -4,31 +4,70 @@ document.getElementById('main-course').style.display = 'none';
 document.getElementById('sides').style.display = 'none';
 document.getElementById('dessert').style.display = 'none';
 
+var mezzeClicked = 0;
+var mainClicked = 0;
+var sidesClicked = 0;
+var dessertClicked = 0;
+
 function mezzeProducts() {
-    document.getElementById('mezze').style.display = 'block';
-    document.getElementById('popular').style.display = 'none';
-    document.getElementById('main-course').style.display = 'none';
-    document.getElementById('sides').style.display = 'none';
-    document.getElementById('dessert').style.display = 'none';
-    var mainElems = document.getElementsByClassName('mezze');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'block';
-        }
-    var mainElems = document.getElementsByClassName('main');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'none';
-        }
-    var mainElems = document.getElementsByClassName('sides');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'none';
-        }
-    var mainElems = document.getElementsByClassName('dessert');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'none';
-        }
+    if (mezzeClicked == 0) {
+        document.getElementById('mezze').style.display = 'block';
+        document.getElementById('popular').style.display = 'none';
+        document.getElementById('main-course').style.display = 'none';
+        document.getElementById('sides').style.display = 'none';
+        document.getElementById('dessert').style.display = 'none';
+        var mainElems = document.getElementsByClassName('mezze');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('main');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'none';
+            }
+        var mainElems = document.getElementsByClassName('sides');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'none';
+            }
+        var mainElems = document.getElementsByClassName('dessert');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'none';
+            }
+        mezzeClicked = 1;
+        mainClicked = 0;
+        sidesClicked = 0;
+        dessertClicked = 0;
+    }
+    else {
+        document.getElementById('mezze').style.display = 'none';
+        document.getElementById('popular').style.display = 'block';
+        document.getElementById('main-course').style.display = 'none';
+        document.getElementById('sides').style.display = 'none';
+        document.getElementById('dessert').style.display = 'none';
+        var mainElems = document.getElementsByClassName('mezze');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('main');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('sides');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('dessert');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        mezzeClicked = 0;
+        mainClicked = 0;
+        sidesClicked = 0;
+        dessertClicked = 0;
+    }
 }
 
 function mainCourseProducts() {
+    if (mainClicked == 0) {
     document.getElementById('main-course').style.display = 'block';
     document.getElementById('popular').style.display = 'none';
     document.getElementById('mezze').style.display = 'none';
@@ -50,52 +89,151 @@ function mainCourseProducts() {
         for (var i=0; i<mainElems.length; i++) {
             mainElems[i].style.display = 'none';
         }
+    mezzeClicked = 0;
+    mainClicked = 1;
+    sidesClicked = 0;
+    dessertClicked = 0;
+    }
+    else {
+        document.getElementById('mezze').style.display = 'none';
+        document.getElementById('popular').style.display = 'block';
+        document.getElementById('main-course').style.display = 'none';
+        document.getElementById('sides').style.display = 'none';
+        document.getElementById('dessert').style.display = 'none';
+        var mainElems = document.getElementsByClassName('mezze');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('main');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('sides');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('dessert');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        mezzeClicked = 0;
+        mainClicked = 0;
+        sidesClicked = 0;
+        dessertClicked = 0;
+    }
+    
 }
 
 function sidesProducts() {
-    document.getElementById('sides').style.display = 'block';
-    document.getElementById('popular').style.display = 'none';
-    document.getElementById('mezze').style.display = 'none';
-    document.getElementById('main-course').style.display = 'none';
-    document.getElementById('dessert').style.display = 'none';
-    var mainElems = document.getElementsByClassName('mezze');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'none';
-        }
-    var mainElems = document.getElementsByClassName('main');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'none';
-        }
-    var mainElems = document.getElementsByClassName('sides');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'block';
-        }
-    var mainElems = document.getElementsByClassName('dessert');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'none';
-        }
+    if (sidesClicked == 0) {
+        document.getElementById('sides').style.display = 'block';
+        document.getElementById('popular').style.display = 'none';
+        document.getElementById('mezze').style.display = 'none';
+        document.getElementById('main-course').style.display = 'none';
+        document.getElementById('dessert').style.display = 'none';
+        var mainElems = document.getElementsByClassName('mezze');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'none';
+            }
+        var mainElems = document.getElementsByClassName('main');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'none';
+            }
+        var mainElems = document.getElementsByClassName('sides');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('dessert');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'none';
+            }
+        mezzeClicked = 0;
+        mainClicked = 0;
+        sidesClicked = 1;
+        dessertClicked = 0;
+    }
+    else {
+        document.getElementById('mezze').style.display = 'none';
+        document.getElementById('popular').style.display = 'block';
+        document.getElementById('main-course').style.display = 'none';
+        document.getElementById('sides').style.display = 'none';
+        document.getElementById('dessert').style.display = 'none';
+        var mainElems = document.getElementsByClassName('mezze');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('main');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('sides');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('dessert');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        mezzeClicked = 0;
+        mainClicked = 0;
+        sidesClicked = 0;
+        dessertClicked = 0;
+    }
 }
 
 function dessertProducts() {
-    document.getElementById('dessert').style.display = 'block';
-    document.getElementById('popular').style.display = 'none';
-    document.getElementById('mezze').style.display = 'none';
-    document.getElementById('main-course').style.display = 'none';
-    document.getElementById('sides').style.display = 'none';
-    var mainElems = document.getElementsByClassName('mezze');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'none';
-        }
-    var mainElems = document.getElementsByClassName('main');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'none';
-        }
-    var mainElems = document.getElementsByClassName('sides');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'none';
-        }
-    var mainElems = document.getElementsByClassName('dessert');
-        for (var i=0; i<mainElems.length; i++) {
-            mainElems[i].style.display = 'block';
-        }
+    if (dessertClicked == 0) {
+        document.getElementById('dessert').style.display = 'block';
+        document.getElementById('popular').style.display = 'none';
+        document.getElementById('mezze').style.display = 'none';
+        document.getElementById('main-course').style.display = 'none';
+        document.getElementById('sides').style.display = 'none';
+        var mainElems = document.getElementsByClassName('mezze');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'none';
+            }
+        var mainElems = document.getElementsByClassName('main');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'none';
+            }
+        var mainElems = document.getElementsByClassName('sides');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'none';
+            }
+        var mainElems = document.getElementsByClassName('dessert');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        mezzeClicked = 0;
+        mainClicked = 0;
+        sidesClicked = 0;
+        dessertClicked = 1;
+    }
+    else {
+        document.getElementById('mezze').style.display = 'none';
+        document.getElementById('popular').style.display = 'block';
+        document.getElementById('main-course').style.display = 'none';
+        document.getElementById('sides').style.display = 'none';
+        document.getElementById('dessert').style.display = 'none';
+        var mainElems = document.getElementsByClassName('mezze');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('main');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('sides');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        var mainElems = document.getElementsByClassName('dessert');
+            for (var i=0; i<mainElems.length; i++) {
+                mainElems[i].style.display = 'block';
+            }
+        mezzeClicked = 0;
+        mainClicked = 0;
+        sidesClicked = 0;
+        dessertClicked = 0;
+    }
 }
