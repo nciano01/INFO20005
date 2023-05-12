99c619a5c0683002d7c28abed2ff960430857287
nicholas <nicholasciano01@gmail.com>
2023-05-12 14:31:32 +1000

    Disable scrolling when nav is active

--- scripts/mobile-nav-activation.js
+++ scripts/mobile-nav-activation.js
@@ -2,10 +2,14 @@ document.getElementsByClassName("hamburger-icon")[0].onclick = showNav;
 
 function showNav() {
     document.getElementsByClassName("mobile-nav")[0].style.display = "block";
+    document.getElementsByTagName("body")[0].style.position = "fixed";
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
 }
 
 document.getElementsByClassName("nav-exit")[0].onclick = hideNav;
 
 function hideNav() {
     document.getElementsByClassName("mobile-nav")[0].style.display = "none";
+    document.getElementsByTagName("body")[0].style.position = "static";
+    document.getElementsByTagName("body")[0].style.overflow = "visible";
 }
