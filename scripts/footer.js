7f1f20a505c1aa7608032a4a6129f838f9b1558b
nicholas <nicholasciano01@gmail.com>
2023-05-12 16:18:55 +1000

    Fixed footer positioning

---
+++ scripts/footer.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", footerHeight);
+window.addEventListener("resize", footerHeight);
+
+function footerHeight() {
+    var navHeight = document.getElementsByTagName("header")[0].scrollHeight;
+    var footerHeight = document.getElementsByTagName("footer")[0].scrollHeight;
+    document.getElementsByClassName("content")[0].style.minHeight = "calc(100vh - " + navHeight + "px - " + footerHeight + "px)";
+}
