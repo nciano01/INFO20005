a6aef2ad169de1af5cc8de80f22ba8ca979acdbd
annadng <annadang2001@gmail.com>
2023-05-25 23:04:04 +1000

    Adjust max width of product list container when resizing window

--- scripts/dynamic-product-list.js
+++ scripts/dynamic-product-list.js
@@ -2,6 +2,12 @@ window.addEventListener("resize", productListShrink);
 
 function productListShrink() {
     if (window.innerWidth < 970) {
-        document.getElementsByClassName("products-cont")[1];
+        document.getElementsByClassName("product-list")[0].style.maxWidth = '720px';
+    } else if (window.innerWidth < 720) {
+        document.getElementsByClassName("product-list")[0].style.maxWidth = '470px';
+    } else if (window.innerWidth < 470){
+        document.getElementsByClassName("product-list")[0].style.maxWidth = '220px';
+    } else {
+        document.getElementsByClassName("product-list")[0].style.maxWidth = '970px';
     }
 }
