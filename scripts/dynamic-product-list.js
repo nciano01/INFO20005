16c63ae0aec4120ad645b1f87a9bb0535825fa04
annadng <annadang2001@gmail.com>
2023-05-26 12:34:37 +1000

    Add && conditions to conditional statements

--- scripts/dynamic-product-list.js
+++ scripts/dynamic-product-list.js
@@ -1,11 +1,11 @@
 window.addEventListener("resize", productListShrink);
 
 function productListShrink() {
-    if (window.innerWidth < 970) {
+    if (window.innerWidth < 970 && window.innerWidth >= 720) {
         document.getElementsByClassName("product-list")[0].style.maxWidth = '720px';
-    } else if (window.innerWidth < 720) {
+    } else if (window.innerWidth < 720 && window.innerWidth >= 470) {
         document.getElementsByClassName("product-list")[0].style.maxWidth = '470px';
-    } else if (window.innerWidth < 470){
+    } else if (window.innerWidth < 470 && window.innerWidth >= 220){
         document.getElementsByClassName("product-list")[0].style.maxWidth = '220px';
     } else {
         document.getElementsByClassName("product-list")[0].style.maxWidth = '970px';
