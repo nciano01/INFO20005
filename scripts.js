f4d0e0f65db97205156fd86ba13f0bd233a41525
annadng <annadang2001@gmail.com>
2023-05-28 22:03:58 +1000

    Delete script file of matching left link cont to right

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// matching right link container width to the left in header
-const rightLinkCont = document.getElementById("right-links");
-const leftLinkCont = document.getElementById("left-links");
-
-const rightLinkContWidth = rightLinkCont.offsetWidth;
-
-leftLinkCont.style.width = rightLinkContWidth + 'px';
