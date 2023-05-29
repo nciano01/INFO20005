117b4f23555697a939fecc1b159fd91aa6eba226
nicholas <nicholasciano01@gmail.com>
2023-05-29 19:58:51 +1000

    Suggested items now update quantity

--- scripts/dynamic-cart.js
+++ scripts/dynamic-cart.js
@@ -147,9 +147,34 @@ function itemAddition() {
     var info = row.parentNode;
     /* grabs element title and price */
     var productInfo = info.getElementsByClassName('suggestion-contents');
-    /* adds to the table */
+    /* reads in cart */
     var table = document.getElementsByClassName('cart-items')[0];
-    table.innerHTML += '<div class="cart-table"> <div class="cart-img img-selection--' + event.currentTarget.id + '"></div> <div class="cart-descrip"><h2 class="in-box-title">' + productInfo[0].innerHTML + '</h2><table class="table-order-actions"><tr><td><div class="quantity quantity--cart"><button class="subtraction">-</button><p>1</p><button class="addition">+</button></div></td></tr><tr><td class="bottom"><h2 class="in-box-title item-price">' + productInfo[1].innerHTML + '</h2></td><td class="square-col"><button class="button--action"><img src="images/modify.png" class="square-button-image"></button></td><td class="square-col"><button class="button--action button-delete"><img src="images/bin.png" class="square-button-image"></button></td></tr></table></div></div>';
+    /* checks if item exists in cart */
+    var productNames = table.getElementsByClassName('item-name');
+    var iToFind = 0;
+    var alreadyThere = 0;
+    for (var i=0; i<productNames.length; i++) {
+        if (productNames[i].innerHTML == productInfo[0].innerHTML) {
+            alreadyThere = 1;
+            iToFind = i;
+        }
+    }
+    /* adds to the table, if necessary */
+    if (alreadyThere == 0) {
+        table.innerHTML += '<div class="cart-table"> <div class="cart-img img-selection--' + event.currentTarget.id + '"></div> <div class="cart-descrip"><h2 class="in-box-title item-name">' + productInfo[0].innerHTML + '</h2><table class="table-order-actions"><tr><td><div class="quantity quantity--cart"><button class="subtraction">-</button><p>1</p><button class="addition">+</button></div></td></tr><tr><td class="bottom"><h2 class="in-box-title item-price">' + productInfo[1].innerHTML + '</h2></td><td class="square-col"><button class="button--action"><img src="images/modify.png" class="square-button-image"></button></td><td class="square-col"><button class="button--action button-delete"><img src="images/bin.png" class="square-button-image"></button></td></tr></table></div></div>';
+    }
+    else {
+        /* seeks out current quantity of item */
+        var relevantProduct = table.getElementsByClassName('cart-descrip')[iToFind];
+        var currQuantity = relevantProduct.getElementsByTagName('p')[0].innerHTML * 1;
+        /* updates quantity and price */
+        currPrice = relevantProduct.getElementsByClassName("item-price")[0].innerHTML.slice(1) * 1;
+        updatedPrice = currPrice + (currPrice / currQuantity);
+        displayPrice = (Math.round(updatedPrice * 100) / 100).toFixed(2);
+        relevantProduct.getElementsByClassName("item-price")[0].innerHTML = "$" + displayPrice;
+        currQuantity += 1;
+        relevantProduct.getElementsByTagName('p')[0].innerHTML = currQuantity;
+    }
     /* re-calculates price */
     priceCalc();
     /* re-calculates cart height */
