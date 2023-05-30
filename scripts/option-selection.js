63566d00950dae1c5ed17132fa3f8f8f200af7f5
annadng <annadang2001@gmail.com>
2023-05-30 16:31:19 +1000

    Add js functions to alternate colors between option buttons

--- scripts/option-selection.js
+++ scripts/option-selection.js
@@ -1,9 +1,43 @@
-var optionButton = document.getElementsByClassName('option-but');
+function smallSelection() {
+    if (document.getElementsByClassName('small')[0].style.borderColor == 'var(--color-tertiary)') {
+        document.getElementsByClassName('small')[0].style.borderColor = 'var(--light-grey)';
+        document.getElementsByClassName('large')[0].style.borderColor = 'var(--color-tertiary)';
+    }
+    else {
+        document.getElementsByClassName('small')[0].style.borderColor = 'var(--color-tertiary)';
+        document.getElementsByClassName('large')[0].style.borderColor = 'var(--light-grey)';
+    }
+}
 
-function selection() {
-    for (var i=0; i<optionButton.length; i++) {
-        if (document.getElementsByClassName('option-but')[i].style.borderColor == 'var(--color-tertiary)') {
-            document.getElementsByClassName('option-but')[i].style.borderColor = 'var(--light-grey)';
-        }
+function largeSelection() {
+    if (document.getElementsByClassName('large')[0].style.borderColor == 'var(--color-tertiary)') {
+        document.getElementsByClassName('large')[0].style.borderColor = 'var(--light-grey)';
+        document.getElementsByClassName('small')[0].style.borderColor = 'var(--color-tertiary)';
+    }
+    else {
+        document.getElementsByClassName('large')[0].style.borderColor = 'var(--color-tertiary)';
+        document.getElementsByClassName('small')[0].style.borderColor = 'var(--light-grey)';
+    }
+}
+
+function pitaSelection() {
+    if (document.getElementsByClassName('pita')[0].style.borderColor == 'var(--color-tertiary)') {
+        document.getElementsByClassName('pita')[0].style.borderColor = 'var(--light-grey)';
+        document.getElementsByClassName('turkish')[0].style.borderColor = 'var(--color-tertiary)';
+    }
+    else {
+        document.getElementsByClassName('pita')[0].style.borderColor = 'var(--color-tertiary)';
+        document.getElementsByClassName('turkish')[0].style.borderColor = 'var(--light-grey)';
+    }
+}
+
+function turkishSelection() {
+    if (document.getElementsByClassName('turkish')[0].style.borderColor == 'var(--color-tertiary)') {
+        document.getElementsByClassName('turkish')[0].style.borderColor = 'var(--light-grey)';
+        document.getElementsByClassName('pita')[0].style.borderColor = 'var(--color-tertiary)';
+    }
+    else {
+        document.getElementsByClassName('turkish')[0].style.borderColor = 'var(--color-tertiary)';
+        document.getElementsByClassName('pita')[0].style.borderColor = 'var(--light-grey)';
     }
 }
