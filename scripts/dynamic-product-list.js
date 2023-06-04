317dd4c326ecca7f0f00bc786ff1c061cbf02473
nicholas <nicholasciano01@gmail.com>
2023-06-04 10:23:26 +1000

    Slighty modified dynamic widths of product page

--- scripts/dynamic-product-list.js
+++ scripts/dynamic-product-list.js
@@ -2,11 +2,11 @@ window.addEventListener("load", productListShrink);
 window.addEventListener("resize", productListShrink);
 
 function productListShrink() {
-    if (window.innerWidth < 970 && window.innerWidth >= 720) {
+    if (window.innerWidth < 1020 && window.innerWidth >= 770) {
         document.getElementsByClassName("product-list")[0].style.maxWidth = '720px';
-    } else if (window.innerWidth < 720 && window.innerWidth >= 470) {
+    } else if (window.innerWidth < 770 && window.innerWidth >= 520) {
         document.getElementsByClassName("product-list")[0].style.maxWidth = '470px';
-    } else if (window.innerWidth < 470 && window.innerWidth >= 220){
+    } else if (window.innerWidth < 520 && window.innerWidth >= 270){
         document.getElementsByClassName("product-list")[0].style.maxWidth = '220px';
     } else {
         document.getElementsByClassName("product-list")[0].style.maxWidth = '970px';
