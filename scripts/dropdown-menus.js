ab75f759a1b54db8e94ba025517df94a5aa7c4f4
nicholas <nicholasciano01@gmail.com>
2023-06-05 18:05:58 +1000

    Further form work

--- scripts/dropdown-menus.js
+++ scripts/dropdown-menus.js
@@ -2,11 +2,21 @@ document.getElementsByClassName("delivery-t1")[0].style.display = "block";
 document.getElementsByClassName("delivery-t2")[0].style.display = "none";
 
 var dropdown = document.getElementById("day-id");
+window.addEventListener("load", checkDay);
 dropdown.addEventListener("change", selectiveDropdown);
+
+/* removes today option if the restaurant is closed when user visits*/
+function checkDay() {
+    var date = new Date();
+    var day = date.getDay();
+    if (day == 1 || day == 2) {
+        dropdown.remove(0);
+    }
+}
 
 function selectiveDropdown() {
     var selection = document.getElementById("day-id").value;
-    if (selection == "monday" || selection == "tuesday" || selection == "wednesday" || selection == "thursday" || selection == "friday") {
+    if (selection == "wednesday" || selection == "thursday" || selection == "friday") {
         document.getElementsByClassName("delivery-t1")[0].style.display = "block";
         document.getElementsByClassName("delivery-t2")[0].style.display = "none";
     } else if (selection == "saturday" || selection == "sunday") {
@@ -18,7 +28,8 @@ function selectiveDropdown() {
         if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5) {
             document.getElementsByClassName("delivery-t1")[0].style.display = "block";
             document.getElementsByClassName("delivery-t2")[0].style.display = "none";
-        } else if (day == 0 || day == 6) {
+        }
+        else if (day == 0 || day == 6) {
             document.getElementsByClassName("delivery-t1")[0].style.display = "none";
             document.getElementsByClassName("delivery-t2")[0].style.display = "block";
         }
