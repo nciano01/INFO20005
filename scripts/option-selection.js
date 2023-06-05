1bcce526dccb32bd45f0441f8a93acbceeea7591
annadng <annadang2001@gmail.com>
2023-06-05 22:59:51 +1000

    Move qty-button functions here and adjust

--- scripts/option-selection.js
+++ scripts/option-selection.js
@@ -1,15 +1,19 @@
 var totalPrice = document.querySelectorAll(".total-price h3")[0];
 
+var clicked = 0
+
 function smallSelection() {
     if (document.getElementsByClassName('small')[0].style.borderColor == 'var(--color-tertiary)') {
         document.getElementsByClassName('small')[0].style.borderColor = 'var(--color-tertiary)';
         document.getElementsByClassName('large')[0].style.borderColor = 'var(--light-grey)';
+        clicked = 0
     } else {
         document.getElementsByClassName('small')[0].style.borderColor = 'var(--color-tertiary)';
         document.getElementsByClassName('small')[0].style.color = 'var(--black)';
         document.getElementsByClassName('large')[0].style.borderColor = 'var(--light-grey)';
         document.getElementsByClassName('large')[0].style.color = 'var(--medium-grey)';
         totalPrice.textContent = "$" + String((Number(totalPrice.textContent.slice(1)) - 5).toFixed(2));
+        clicked = 0
     }
 }
 
@@ -17,12 +21,14 @@ function largeSelection() {
     if (document.getElementsByClassName('large')[0].style.borderColor == 'var(--color-tertiary)') {
         document.getElementsByClassName('large')[0].style.borderColor = 'var(--color-tertiary)';
         document.getElementsByClassName('small')[0].style.borderColor = 'var(--light-grey)';
+        clicked = 1
     } else {
         document.getElementsByClassName('large')[0].style.borderColor = 'var(--color-tertiary)';
         document.getElementsByClassName('large')[0].style.color = 'var(--black)';
         document.getElementsByClassName('small')[0].style.borderColor = 'var(--light-grey)';
         document.getElementsByClassName('small')[0].style.color = 'var(--medium-grey)';
         totalPrice.textContent = "$" + String((Number(totalPrice.textContent.slice(1)) + 5).toFixed(2));
+        clicked = 1
     }
 }
 
@@ -48,5 +54,42 @@ function turkishSelection() {
         document.getElementsByClassName('turkish')[0].style.color = 'var(--black)';
         document.getElementsByClassName('pita')[0].style.borderColor = 'var(--light-grey)';
         document.getElementsByClassName('pita')[0].style.color = 'var(--medium-grey)';
+    }
+}
+
+/* qty button */
+
+var quantity = document.getElementsByClassName("current-quantity")[0];
+
+var curQuantity = Number(quantity.textContent);
+var curPrice = Number(totalPrice.textContent.slice(1));
+
+function plus() {
+    if (clicked == 1) {
+        curQuantity += 1;
+        quantity.textContent = String(curQuantity);
+
+        totalPrice.textContent = "$" + String((Number(totalPrice.textContent.slice(1)) + 19.5).toFixed(2));
+    } else {
+        curQuantity += 1;
+        quantity.textContent = String(curQuantity);
+
+        totalPrice.textContent = "$" + String((Number(totalPrice.textContent.slice(1)) + 14.5).toFixed(2));
+    }
+}
+
+function minus() {
+    if (quantity.textContent >= "2") {
+        if (clicked == 1) {
+            curQuantity -= 1;
+            quantity.textContent = String(curQuantity);
+    
+            totalPrice.textContent = "$" + String((Number(totalPrice.textContent.slice(1)) - 19.5).toFixed(2));
+        } else {
+            curQuantity -= 1;
+            quantity.textContent = String(curQuantity);
+    
+            totalPrice.textContent = "$" + String((Number(totalPrice.textContent.slice(1)) - 14.5).toFixed(2));
+        }
     }
 }
